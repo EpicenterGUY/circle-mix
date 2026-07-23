283089c3f57c3ef83fea515ff588d50768329c75
EpicenterGUY <mintbamtol7@gmail.com>
2026-07-23 21:52:47 +0900

    test: normalize Windows line endings in updater workflow regression

--- tests/desktop-updater.test.js
+++ tests/desktop-updater.test.js
@@ -10,7 +10,7 @@ const rust=fs.readFileSync(path.join(root,'src-tauri/src/lib.rs'),'utf8');
 const capability=JSON.parse(fs.readFileSync(path.join(root,'src-tauri/capabilities/default.json'),'utf8'));
 const updater=fs.readFileSync(path.join(root,'src/desktop-updater.js'),'utf8');
 const releaseConfig=JSON.parse(fs.readFileSync(path.join(root,'src-tauri/tauri.release.conf.json'),'utf8'));
-const releaseWorkflow=fs.readFileSync(path.join(root,'.github/workflows/windows-updater-release.yml'),'utf8');
+const releaseWorkflow=fs.readFileSync(path.join(root,'.github/workflows/windows-updater-release.yml'),'utf8').replace(/\r\n/g,'\n');
 
 assert.equal(tauri.version,'0.9.36');
 assert.equal(tauri.app?.withGlobalTauri,true,'desktop updater UI needs the global Tauri core bridge');
