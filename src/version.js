7da6264a4da68120e7ebd310c93f29c6560250f1
EpicenterGUY <mintbamtol7@gmail.com>
2026-07-23 19:33:27 +0900

    refactor: share web release metadata

--- src/version.js
+++ src/version.js
@@ -1,4 +1,8 @@
-window.CircleMixVersion = {
-  version: "0.9.30",
-  buildDate: "2026-07-21"
-};
+(function(root){
+  "use strict";
+  root.CircleMixVersion = Object.freeze({
+    version: "0.9.30",
+    buildDate: "2026-07-23",
+    cacheRevision: "20260723-pwa-update-foundation"
+  });
+})(typeof window !== "undefined" ? window : self);
