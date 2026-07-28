5939f7d7fa48a749086449a695ea987f9b764ad4
EpicenterGUY <mintbamtol7@gmail.com>
2026-07-28 23:06:17 +0900

    update web cache revision 0.9.49

--- src/version.js
+++ src/version.js
@@ -1,8 +1,8 @@
 (function(root){
   "use strict";
   root.CircleMixVersion = Object.freeze({
-    version: "0.9.33",
+    version: "0.9.49",
     buildDate: "2026-07-28",
-    cacheRevision: "20260728-all-platforms-0.9.46"
+    cacheRevision: "20260728-library-scroll-sort-0.9.49"
   });
 })(typeof window !== "undefined" ? window : self);
