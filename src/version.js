e8b0d563ea58664bd9a99440343ee9f549bf65f7
EpicenterGUY <mintbamtol7@gmail.com>
2026-07-28 23:24:49 +0900

    keep web version line while refreshing 0.9.49 cache

--- src/version.js
+++ src/version.js
@@ -1,7 +1,7 @@
 (function(root){
   "use strict";
   root.CircleMixVersion = Object.freeze({
-    version: "0.9.49",
+    version: "0.9.33",
     buildDate: "2026-07-28",
     cacheRevision: "20260728-library-scroll-sort-0.9.49"
   });
