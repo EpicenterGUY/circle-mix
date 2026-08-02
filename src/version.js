16399ac2d613d8eafb696379eebb28ff088ed1b5
EpicenterGUY <mintbamtol7@gmail.com>
2026-08-02 18:21:13 +0900

    release: bump web and PWA to 0.9.34

--- src/version.js
+++ src/version.js
@@ -1,8 +1,8 @@
 (function(root){
   "use strict";
   root.CircleMixVersion = Object.freeze({
-    version: "0.9.33",
-    buildDate: "2026-07-28",
-    cacheRevision: "20260728-library-scroll-sort-0.9.49"
+    version: "0.9.34",
+    buildDate: "2026-08-02",
+    cacheRevision: "20260802-unlimited-content-0.9.53"
   });
 })(typeof window !== "undefined" ? window : self);
